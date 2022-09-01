99674ab78c5825f12af318a10bc1f42b9cb1c593
Alex Biba <alexbiba@gmail.com>
2022-09-01 17:45:59 -0400

    Integrate with chrono

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
+import { parseDate } from 'chrono-node';
+
 chrome.webNavigation.onCompleted.addListener(({ tabId, frameType }) => {
     if (frameType !== "outermost_frame") return;
     chrome.scripting.executeScript({
         target: { tabId },
         func: function() {
             document.body.addEventListener("mouseup", e => {
-                const selection = window.getSelection();
-                const selectedText = selection.toString();
-                if (selectedText) {
-                    console.log(`${selectedText} is ${selectedText} in your timezone`)
-                }
+                const selectedText = window.getSelection().toString();
+                if (selectedText) chrome.runtime.sendMessage(selectedText, console.log);
                 e.stopPropagation();
             });
         }
     });
+});
+
+chrome.runtime.onMessage.addListener((msg, sender, callback) => {
+    callback(parseDate(msg)?.toLocaleString("en-US", {timeZone: "America/New_York"}));
 });
